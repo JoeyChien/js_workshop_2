7cdef0dd8843eebea690d8e577c0407a872faa42
Joey <aileen60222@gmail.com>
2020-05-05 17:13:35 +0800

    sweetalert跟select2任務做完

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,49 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-//bootstrap
+
+//bootstrap js
 import 'bootstrap'
+//bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import $ from 'jquery'
+// $是全域變數
+window.$ = $
+
+// import 'tempusdominus-bootstrap-4'
+// import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
+
+import 'select2'
+import 'select2/dist/css/select2.css'
+
+import 'sweetalert2'
+import Swal from 'sweetalert2'
+window.Swal = Swal
+
+$(document).on('turbolinks:load', function () {
+  $('.multiselect').select2({
+    tags: true,
+    tokenSeparators: [',', ' ']
+  });
+  $('input[type="submit"]').on("click", function(e){
+    e.preventDefault();
+    let myForm = $(this).parents('form')
+    console.log(myForm)
+    Swal.fire({
+      title: 'Are you sure?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, do it!'
+      }).then((result) => {
+        if (result.value) {  
+          myForm.submit();
+        } else{
+          myForm.find('input[type="submit"]').remove('disabled')
+        }
+    })
+  })  
+});
+
